98c966cb03377614d54456b54ae3808f94c8f718
Marc-Armillas <marcarmillas@gmail.com>
2024-09-15 22:57:26 +0200

    active storage migrations

---
+++ db/migrate/20240915182505_add_name_to_users.rb
@@ -0,0 +1,8 @@
+class AddNameToUsers < ActiveRecord::Migration[7.2]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :phone, :string
+    add_column :users, :alias, :integer
+  end
+end
