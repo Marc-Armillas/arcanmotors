da8866eb820754883164b7920537ddd062574678
Marc-Armillas <marcarmillas@gmail.com>
2024-09-24 05:26:25 +0200

    amz production credentials

---
+++ config/initializers/aws.rb
@@ -0,0 +1,8 @@
+Aws.config.update({
+  region: Rails.application.credentials.dig(:aws, :region),
+  credentials: Aws::Credentials.new(
+    Rails.application.credentials.dig(:aws, :access_key_id),
+    Rails.application.credentials.dig(:aws, :secret_access_key)
+  )
+})
+
