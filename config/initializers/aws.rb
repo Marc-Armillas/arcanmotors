fba5e395d046f8e62efad1c85a6099904a8653e7
Marc-Armillas <marcarmillas@gmail.com>
2024-09-24 14:19:56 +0200

    aws-s3 config

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,8 +1,8 @@
-# Aws.config.update({
-#   region: Rails.application.credentials.dig(:aws, :region),
-#   credentials: Aws::Credentials.new(
-#     Rails.application.credentials.dig(:aws, :access_key_id),
-#     Rails.application.credentials.dig(:aws, :secret_access_key)
-#   )
-# })
-#
+# Initialize AWS configuration
+unless Rails.env.test?
+  Aws.config = {
+    access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
+    secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
+    region: Rails.application.credentials.dig(:aws, :region)
+  }
+end
