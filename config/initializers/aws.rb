c05b1e1a1345b4ba9b45fd7cbbf4866059757d39
Marc-Armillas <marcarmillas@gmail.com>
2024-10-20 02:53:17 +0200

    tab logo

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,8 +1,8 @@
-# Initialize AWS configuration
-unless Rails.env.test?
-  Aws.config = {
-    access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
-    secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
-    region: Rails.application.credentials.dig(:aws, :region)
-  }
-end
+# # Initialize AWS configuration
+# unless Rails.env.test?
+#   Aws.config = {
+#     access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
+#     secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
+#     region: Rails.application.credentials.dig(:aws, :region)
+#   }
+# end
