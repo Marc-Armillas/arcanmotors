ac39a1317c213d95b2c493af181f1cd981ac218b
Marc-Armillas <marcarmillas@gmail.com>
2024-10-23 00:10:24 +0200

    refactor admin controller method

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,5 @@
 class AdminController < ApplicationController
   layout "admin"
-  # before_action :authenticate_user!
   before_action :check_admin
 
   def index; end
@@ -8,8 +7,8 @@ class AdminController < ApplicationController
   private
 
   def check_admin
-    unless current_user.present? && current_user.admin?
-      redirect_to root_path, alert: 'Access denied'
+    unless current_user&.admin?
+      redirect_to root_path # , alert: 'Access denied'
     end
   end
 end
