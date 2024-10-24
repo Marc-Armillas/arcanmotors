46103c754126ae798bd9a7857e01d2681ff6a1c9
Marc-Armillas <marcarmillas@gmail.com>
2024-10-24 16:39:55 +0200

    add cars url to be indexed

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -4,5 +4,6 @@ SitemapGenerator::Sitemap.create do
   add "/cars", changefreq: "daily", priority: 0.9
   add "/about", changefreq: "monthly"
   add "/contact", changefreq: "monthly"
-  # Añade más rutas aquí
+
+  Car.find_each { |car| add car_path(car), lastmod: car.updated_at, changefreq: "weekly", priority: 0.8 }
 end
