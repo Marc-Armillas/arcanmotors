6d64d9ff81f9a30f165ae7375ca7b2e70b11c237
Marc-Armillas <marcarmillas@gmail.com>
2024-10-24 17:03:59 +0200

    add more urls to be indexed

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -2,8 +2,10 @@ SitemapGenerator::Sitemap.default_host = "https://arcanmotors.es"
 
 SitemapGenerator::Sitemap.create do
   add "/cars", changefreq: "daily", priority: 0.9
-  add "/about", changefreq: "monthly"
-  add "/contact", changefreq: "monthly"
+  add "/about_us", changefreq: "monthly"
+  add "/legal", changefreq: "weekly"
+  add "/cookies_policy", changefreq: "weekly"
+  # add "/contact", changefreq: "monthly"
 
   Car.find_each { |car| add car_path(car), lastmod: car.updated_at, changefreq: "weekly", priority: 0.8 }
 end
